a381d3860f8e57d902f62d7f790f1f3956facdd3
jura <jirapanek@gmail.com>
2018-06-06 22:45:44 +0200

    Added IProtocol interface, ProtocolPacket class.

---
+++ JablotronProtocol/IProtocol.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JablotronProtocol
+{
+    enum State { Ok = 0x01, Error = 0x02, Unknown = 0xFF };
+    enum Type { Bool = 0x01, Int8 = 0x10, Int16 = 0x11, String = 0x20 };
+    enum Error { Ok = 0x00, UnknownId = 0x10, UnknownType = 0x20, DataLengthError = 0x80, UnknownError = 0xFF };
+
+    interface IProtocol
+    {
+        State State { get; set; }
+
+        int ID { get; set; }
+
+        Type Type { get; set; }
+
+        Error Error { get; set; }
+
+        int ReadSettings();
+        void WriteSettings(int settings);
+    }
+}
---
+++ JablotronProtocol/ProtocolPacket.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JablotronProtocol
+{
+    public class ProtocolPacket
+    {
+        public const byte Sop = 0x80;
+        public const byte Eop = 0x80;
+
+        public byte SOP { get; set; }
+        public byte Len { get; set; }
+        public byte[] Data { get; set; }
+        public UInt16 CRC { get; set; }
+        public byte EOP { get; set; }
+    }
+}
